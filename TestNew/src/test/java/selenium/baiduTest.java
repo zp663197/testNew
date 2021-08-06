e011961525f4025cdc66f5774b09a1ed1ced4694
zhang.peng <zhang.peng84@byd.com>
2021-08-06 15:24:10 +0800

    test

--- TestNew/src/test/java/selenium/baiduTest.java
+++ TestNew/src/test/java/selenium/baiduTest.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -15,12 +16,16 @@ private static Map<String, Object> vars;
 static JavascriptExecutor js;
 @BeforeClass
 public  static void  setUp() {
- System.setProperty("webdriver.firefox.bin","D:\\firefox\\firefox.exe");
- System.setProperty("webdriver.gecko.driver","D:\\firefox\\geckodriver.exe");
- driver = new FirefoxDriver();
- js = (JavascriptExecutor) driver;
- vars = new HashMap<String, Object>();
+ //System.setProperty("webdriver.firefox.bin","C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+ //System.setProperty("webdriver.gecko.driver","C:\\Program Files\\Mozilla Firefox\\geckodriver\\geckodriver.exe");
+ //driver = new FirefoxDriver();
+ //js = (JavascriptExecutor) driver;
+ //vars = new HashMap<String, Object>();
 
+ System.setProperty("webdriver.chrome.bin","D:\\firefox\\firefox.exe");
+ System.setProperty("webdriver.gecko.driver","C:\\Users\\byd_lenovo\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
+ driver = new ChromeDriver();
+ js = (JavascriptExecutor) driver;
 }
 @Test
 public void test1() {
